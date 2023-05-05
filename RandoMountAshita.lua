e400013e78c1f6f0551e620b7645d345e4623345
Ookie <erkamerf@yahoo.com>
2023-05-05 17:06:29 -0400

    Added randomseed function for randomness

--- RandoMountAshita.lua
+++ RandoMountAshita.lua
@@ -5,6 +5,12 @@ _addon.author   = 'Harissa';
 _addon.name     = 'randomount';
 _addon.version  = '0.0.0';
 
+function initialize_myMounts()
+    
+    math.randomseed(os.time())
+
+end
+
 -- Define a function to pick a random mount and summon it
 function SummonRandomMount()
     -- Initialize an empty table to store the player's mounts
@@ -33,7 +39,6 @@ function SummonRandomMount()
     -- Check if the player has any mounts
     if (#mounts > 0) then
         -- Select a random mount from the table
-        -- TODO: This is the correct random function, but we forgot to seed
         local mountIndex = math.random(1, #mounts)
         local mountId = mounts[mountIndex]
 
@@ -44,6 +49,8 @@ function SummonRandomMount()
         print("You have no mounts.")
     end
 end
+
+initialize_myMounts()
 
 -- Register a command to summon a random mount
 ashita.register_event("command", function(cmd, ntype)
